b9e0b8154b1bee0377af4b72c877bc46fb1a6325
beslow <549174542@qq.com>
2020-02-10 22:59:22 +0800

    ApplicationController

--- app/controllers/eric_weixin/wz/xiaodian/orders_controller.rb
+++ app/controllers/eric_weixin/wz/xiaodian/orders_controller.rb
@@ -1,4 +1,4 @@
-class EricWeixin::Wz::Xiaodian::OrdersController < EricWeixin::Cms::BaseController
+class EricWeixin::Wz::Xiaodian::OrdersController < ApplicationController
   def index
     @orders = ::EricWeixin::Xiaodian::Order.all
     @orders = @orders.where("openid = ?", params[:openid]).order('order_create_time DESC') unless params[:openid].blank?
